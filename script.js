a0aae11bbad07239abdb8f39053c51449643a7f7
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-03 21:26:23 +0200

    added delete action for each task in list

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const onEnter = (e) => {
     }
 }
 
+const deleteTask = (e) => {
+    e.target.parentElement.remove();
+}
+
 const createTask = () => {
     let task = document.getElementById('user-to-do-input').value;
     let toDoList = document.querySelector('.to-do-list-items');
@@ -15,7 +19,7 @@ const createTask = () => {
                     <div class="check"></div>
                     <p class="m-0 p-0">${task[0].toUpperCase() + task.slice(1)}</p>
                 </div>
-                <div class="delete-item">
+                <div class="delete-item" onclick="deleteTask(event)">
                     X
                 </div>
          </div>
