4c508fa1098779e94a806de9768e7a0c6f5c4781
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-03 23:33:57 +0200

    made changes to structure to better fir a title an details & completed delete action

--- script.js
+++ script.js
@@ -11,23 +11,32 @@ const deleteTask = (e) => {
 
 const createTask = () => {
     let task = document.getElementById('user-input-title').value;
+    let details = document.getElementById('user-input-details').value;
     let toDoList = document.querySelector('.to-do-list-items');
-
-    // let taskItem = `
-    //     <div class="todo-item-container">
-    //             <div class="todo-item">
-    //                 <div class="check"></div>
-    //                 <p class="m-0 p-0">${task[0].toUpperCase() + task.slice(1)}</p>
-    //             </div>
-    //             <div class="task-description">
-    //             heloooo
-    //             </div>
-    //             <div class="delete-item" onclick="deleteTask(event)">
-    //                 X
-    //             </div>
-    //      </div>
-    // `;
+    let taskItem = `
+        <div class="individual-task-container">
+                <div class="todo-item-container">
+                    <div class="check"></div>
+                    <!-- <div class="tasks-container"> -->
+                    <div class="task-full-info">
+                        <div class="task-details">
+                            <div class="todo-item">
+                                ${task[0].toUpperCase() + task.slice(1)}
+                            </div>
+                        </div>
+                        <div class="task-description">
+                            ${details[0].toUpperCase() + details.slice(1)}
+                        </div>
+                    </div>
+                </div>
+                <div class="delete-item" onclick="deleteTask(event)">
+                    X
+                </div>
+            </div>
+    `;
+   
     toDoList.insertAdjacentHTML('afterbegin', taskItem);
-    document.getElementById('user-to-do-input').value = "";
+    document.getElementById('user-input-title').value = "";
+    document.getElementById('user-input-details').value = "";
 }
 
