acd35140df77ec41a6f745c6340608a7cd464c55
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-03 21:17:11 +0200

    changed order of list creation to start from the top and added capitalization to input value retrieved

--- script.js
+++ script.js
@@ -8,18 +8,19 @@ const onEnter = (e) => {
 const createTask = () => {
     let task = document.getElementById('user-to-do-input').value;
     let toDoList = document.querySelector('.to-do-list-items');
+
     let taskItem = `
         <div class="todo-item-container">
                 <div class="todo-item">
                     <div class="check"></div>
-                    <p class="m-0 p-0">${task}</p>
+                    <p class="m-0 p-0">${task[0].toUpperCase() + task.slice(1)}</p>
                 </div>
                 <div class="delete-item">
                     X
                 </div>
          </div>
     `;
-    toDoList.insertAdjacentHTML('beforeend', taskItem);
-    // console.log();
+    toDoList.insertAdjacentHTML('afterbegin', taskItem);
+    document.getElementById('user-to-do-input').value = "";
 }
 
