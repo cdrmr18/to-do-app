fc1922ddfbeb97aad8e5257bdad654aad2215259
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-04 14:44:29 +0200

    added draggable feature to move between in progress and completed

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+const draggableInProgress = document.querySelector(".to-do-list-items");
+const draggableCompleted = document.getElementById("complete-tasks");
+
+new Sortable(draggableInProgress, {
+    group: 'shared', // set both lists to same group
+    animation: 150
+});
+
+new Sortable(draggableCompleted, {
+    group: 'shared',
+    animation: 150
+});
 // selectors
 const toDoButton = document.getElementById('to-do-button');
 
@@ -25,27 +38,31 @@ const createTask = (e) => {
     let toDoList = document.querySelector('.to-do-list-items');
     let newTaskItem = `
         <div class="individual-task-container mt-1">
-                    <div class="todo-item-container">
-                        <div class="check" onclick="markComplete(event)"></div>
-                        <div class="task-full-info">
-                            <div class="task-details">
-                                <div class="todo-item">
-                                   ${task[0].toUpperCase() + task.slice(1)}
-                                </div>
-                            </div>
-                            <div class="task-description">
-                              ${(details) ? details[0].toUpperCase() + details.slice(1) : ""}
-                            </div>
+            <div class="todo-item-container">
+                <div class="check" onclick="markComplete(event)"></div>
+                <div class="task-full-info">
+                    <div class="task-details">
+                        <div class="todo-item">
+                             ${task[0].toUpperCase() + task.slice(1)}
                         </div>
                     </div>
-                    <div class="delete-item" onclick="deleteTask(event)">
-                        <button class="btn delete-button-bg">🗑️</button>
+                    <div class="task-description">
+                        ${(details) ? details[0].toUpperCase() + details.slice(1) : ""}
                     </div>
                 </div>
+            </div>
+            <div onclick="deleteTask(event)">
+                <button class="btn delete-button-bg">🗑️</button>
+            </div>
+            <div class="drag-icon">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
     `;
    
     toDoList.insertAdjacentHTML('beforeend', newTaskItem);
     document.getElementById('user-input-title').value = "";
     document.getElementById('user-input-details').value = "";
 }
-
+// ${task[0].toUpperCase() + task.slice(1)}
+//  ${(details) ? details[0].toUpperCase() + details.slice(1) : ""}
